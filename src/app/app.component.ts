c595ab579971371d73a190c506c67b15d76a0f18
FabriJuncal <fabriciojuncal@micam.com.ar>
2021-09-30 17:34:53 -0300

    Se agregó componente:  Split Pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interface';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  // Creamos la variable de tipo "Observable" y le indicamos que se inicializará como un array vacio
+  // Observable: es un tipo de variable que se utiliza para almacenar los datos de las peticiones y
+  //            tener a mano siempre información y no tener que acceder al servidor cada vez que lo necesitemos
+  componentes: Observable<Componente[]>;
+
+  constructor(private dataService: DataService) {}
+
+  onSplitPaneVisible(){
+    // Realizamos una petición al archivo "assets/data/menu.json"
+    // Donde en un archivo .json se almacenarán de manera estatica los nombres, iconos y rutas de los Menus
+    this.componentes = this.dataService.getMenuOptions();
+  }
 }
